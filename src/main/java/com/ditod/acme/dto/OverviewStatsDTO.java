b79422a5fb67d3f9e22479acef6255efc30d9416
Orlando Diaz <orlandodiazconde@gmail.com>
2024-01-31 08:59:04 -0500

    Add extra fields to overview stats

--- src/main/java/com/ditod/acme/dto/OverviewStatsDTO.java
+++ src/main/java/com/ditod/acme/dto/OverviewStatsDTO.java
@@ -1,8 +1,14 @@
 package com.ditod.acme.dto;
 
+import com.ditod.acme.model.Revenue;
+
+import java.util.List;
+
 public record OverviewStatsDTO(
         Long invoiceCount,
         Long customerCount,
         Integer totalPaidInvoices,
-        Integer totalPendingInvoices) {
+        Integer totalPendingInvoices,
+        List<Revenue> revenues,
+        List<InvoiceDetailsDTO> latestInvoices) {
 }
--- src/main/java/com/ditod/acme/service/OverviewService.java
+++ src/main/java/com/ditod/acme/service/OverviewService.java
@@ -1,28 +1,39 @@
 package com.ditod.acme.service;
 
+import com.ditod.acme.dto.InvoiceDetailsDTO;
 import com.ditod.acme.dto.InvoiceTotalByStatusDTO;
 import com.ditod.acme.dto.OverviewStatsDTO;
+import com.ditod.acme.model.Revenue;
 import com.ditod.acme.repository.CustomerRepository;
+import com.ditod.acme.repository.RevenueRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OverviewService {
     private final InvoiceService invoiceService;
     private final CustomerRepository customerRepository;
+    private final RevenueRepository revenueRepository;
 
     public OverviewService(InvoiceService invoiceService,
-                           CustomerRepository customerRepository) {
+                           CustomerRepository customerRepository,
+                           RevenueRepository revenueRepository) {
         this.invoiceService = invoiceService;
         this.customerRepository = customerRepository;
+        this.revenueRepository = revenueRepository;
     }
 
     public OverviewStatsDTO findOverviewStats() {
-        InvoiceTotalByStatusDTO invoiceTotalByStatus =
-                invoiceService.findInvoiceTotalByStatus();
         Long invoiceCount = invoiceService.count();
         Long customerCount = customerRepository.count();
+        InvoiceTotalByStatusDTO invoiceTotalByStatus =
+                invoiceService.findInvoiceTotalByStatus();
+        List<InvoiceDetailsDTO> latestInvoices = invoiceService.findLatestInvoices();
+        List<Revenue> revenues = revenueRepository.findAll();
+
         return new OverviewStatsDTO(invoiceCount, customerCount,
                 invoiceTotalByStatus.getPaidInvoicesTotal(),
-                invoiceTotalByStatus.getPendingInvoicesTotal());
+                invoiceTotalByStatus.getPendingInvoicesTotal(), revenues, latestInvoices);
     }
 }
